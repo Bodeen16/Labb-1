51e02a5264873bdf21da5012ae284411d71c1ce9
Bodeen16 <edvin.boden@gmail.com>
2026-09-15 18:12:11 +0200

    klar med 3b

--- uppgift3.py
+++ uppgift3.py
@@ -1,6 +1,6 @@
 import numpy as np 
 import matplotlib.pyplot as plt 
-
+"""
 #Uppgift a; 
 f = lambda x: x**3 * np.exp(x)
 igranser = [0, 2]
@@ -25,3 +25,27 @@ print("Exakt värde:", I_exakt)
 #Från utskriften ser vi att det beräknadet värdet skiljer sig något från det exakta värdet men att det fortfarande är tillräckligt nära för att vi ska kunna verifiera att funktionen fungerar.
 #Beräknat värde: 20.7830380953846
 #Exakt värde: 20.7781121978613 
+"""
+#uppgfift b;
+f = lambda x: x**3 * np.exp(x)
+igranser = [0, 2]
+n = 512
+
+def trapets(f, n, igranser):
+    a, b = igranser
+    h = (b-a)/n
+    x = np.linspace(a, b, n+1)
+    fx = f(x)
+
+    Th = h*(fx[0]/2 + np.sum(fx[1:-1]) + fx[-1]/2)
+    return Th
+
+while n > 1:
+    I_trapets = trapets(f, n, igranser)
+    I_exakt = 6 + 2 *np.exp(2)
+    print("n:", n)
+    print("fel:", abs(I_trapets - I_exakt))
+    print(" ")
+    n = n // 2
+
+#vi ser att när n halveras så ungefär multipliseras felet med 4. vilket följer att approxamationsfelet är Ch^2 eller med andre ord att e[h]/e[h/2] = 4.
