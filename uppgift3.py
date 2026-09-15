e519bd56412604059353d432da3bbd85d8aa5a0e
williamemilsson <williamemilsson@gmail.com>
2026-09-15 17:35:55 +0200

    Uppgift 3a klar

---
+++ uppgift3.py
@@ -0,0 +1,27 @@
+import numpy as np 
+import matplotlib.pyplot as plt 
+
+#Uppgift a; 
+f = lambda x: x**3 * np.exp(x)
+igranser = [0, 2]
+n = 100
+
+def trapets(f, n, igranser):
+    a, b = igranser
+    h = (b-a)/n
+    x = np.linspace(a, b, n+1)
+    fx = f(x)
+
+    Th = h*(fx[0]/2 + np.sum(fx[1:-1]) + 0.5 * fx[-1]/2)
+    print("Th=", Th)
+    return Th
+
+I_trapets = trapets(f, n, igranser)
+I_exakt = 6 + 2 *np.exp(2)
+
+print("Beräknat värde:", I_trapets)
+print("Exakt värde:", I_exakt)
+
+#Från utskriften ser vi att det beräknadet värdet skiljer sig något från det exakta värdet men att det fortfarande är tillräckligt nära för att vi ska kunna verifiera att funktionen fungerar.
+#Beräknat värde: 20.487475851427376
+#Exakt värde: 20.7781121978613
