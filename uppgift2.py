e063faa1b19eaeb45f279950be2dbf2b741dc6f2
williamemilsson <williamemilsson@gmail.com>
2026-09-15 10:47:09 +0200

    Gjorde uppgift 2c

--- uppgift2.py
+++ uppgift2.py
@@ -81,5 +81,79 @@ plt.grid()
 plt.show()
 """
 
-#Uppgift c;
 
+#Uppgift c;
+N = len(t)
+
+c = np.array([194.56324931826524, 3.4878236585092433, -1.095820432393067, 3.480988627461247, 8.0], dtype=float)
+
+tol = 1e-10
+diff = 1
+it = 0
+maxiter = 300
+
+while diff > tol and maxiter > it:
+    d0 = c[0]
+    d1 = c[1]
+    d2 = c[2]
+    d3 = c[3]
+    L = c[4]
+
+    f = lambda t, L: d0 + d1*t + d2*np.sin((2*np.pi*t)/L) + d3*np.cos((2*np.pi*t)/L)
+
+    F = f(t, L) - y 
+
+    J = np.column_stack((
+        t**0,
+        t,
+        np.sin((2*np.pi*t)/L),
+        np.cos((2*np.pi*t)/L),
+        -(2*np.pi*t*d2/L**2)*np.cos((2*np.pi*t)/L) + (2*np.pi*t*d3/L**2)*np.sin((2*np.pi*t)/L)
+    ))
+
+    JTJ = J.T @ J 
+    JTF = J.T @ F
+
+    delta = np.linalg.solve(JTJ, -JTF)
+
+    c = c + delta
+
+    diff = np.max(np.abs(delta))
+    it += 1
+
+d0 = c[0]
+d1 = c[1]
+d2 = c[2]
+d3 = c[3]
+L = c[4] 
+
+f = lambda t, L: d0 + d1*t + d2*np.sin((2*np.pi*t)/L) + d3*np.cos((2*np.pi*t)/L)
+
+r = f(t, L) - y
+
+ERMS = np.sqrt(np.sum(r**2)/N)
+
+print("d0 =", d0)
+print("d1 =", d1)
+print("d2 =", d2)
+print("d3 =", d3)
+print("L =", L)
+print("RMS-felet =", ERMS)
+
+plt.plot(t + 1980, y)
+plt.plot(t + 1980, f(t, L))
+plt.grid()
+plt.show()
+
+plt.plot(t + 1980, r)
+plt.grid()
+plt.show()
+
+
+#Uppgift d; 
+
+#Vid körning av de tre programmen a-c får vi följande RMS-fel:
+#Uppgift a: RMS-felet = 4.130967604665237
+#Uppgift b: RMS-felet = 3.2131915718048005
+#Uppgift c: RMS-felet = 3.198098527008681
+#Vi kan alltså konstatera att modell c gav oss det lägsta RMS-felet. 
