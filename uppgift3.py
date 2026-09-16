9d36e8195233f5f1a107a733886f11fc75997839
williamemilsson <williamemilsson@gmail.com>
2026-09-16 17:33:27 +0200

    Uppgift d gjord

--- uppgift3.py
+++ uppgift3.py
@@ -30,7 +30,7 @@ print("Skillnad: ", np.abs(I_trapets - I_exakt))
 """
 
 
-
+"""
 #uppgfift b;
 f = lambda x: x**3 * np.exp(x)
 igranser = [0, 2]
@@ -59,7 +59,7 @@ while 512 >= n:
     n = n * 2
 
 #vi ser att när n fördubblas så divideras ungefär felet med 4. vilket följer att approxamationsfelet är Ch^2 eller med andre ord att e[h]/e[h/2] = 4.
-
+"""
 
 """
 #Uppgift c;
@@ -72,4 +72,29 @@ print("Skattad total energi:", Th)
 #Vi får då utskriften: Skattad total energi: 277.55.
 """
 
+
 #Uppgift d; 
+ft = np.array([12.00, 15.10, 19.01, 23.92, 30.11, 37.90, 47.70, 60.03, 75.56])
+h = 1
+Th=1
+eh =1
+while h < 9:
+    fta = []
+    i = 0
+    while i < 9:
+        fta.append(ft[i])
+        i = i+h
+    Thg = Th
+    Th = h*(fta[0]/2 + np.sum(fta[1:-1]) + fta[-1]/2)
+    ehg=eh
+    eh = np.abs(Thg-Th)
+    Nog = np.log2(eh/ehg)
+    print(Nog)
+    h = h*2
+
+#Vi får utskriften: 
+#1.976087380055654
+#1.9289574937830132
+
+#Vi ser att noggranhetsordning stämmer hyfsat med vad den ska vara för trapetsregeln i teorin där p = 2.
+
